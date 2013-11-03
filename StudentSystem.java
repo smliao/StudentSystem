c0bdeab015bbbeeeaf2f25f92fb842396b1cc1fe
smliao <stevenliao29@gmail.com>
2013-11-03 13:37:55 -0800

    Add new buttons and comments, Still working on trigger

--- StudentSystem.java
+++ StudentSystem.java
@@ -14,6 +14,7 @@ import java.awt.event.WindowEvent;
 import java.sql.*;
 import java.util.ArrayList;
 
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -32,15 +33,15 @@ public class StudentSystem extends JFrame
 {
    // JDBC driver, database URL, username and password
    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-   static final String DATABASE_URL = "jdbc:mysql://localhost/StudentSystem";
-   static final String DB_URL = "jdbc:mysql://localhost/";
    static final String USERNAME= "root";
    static final String PASSWORD= "password";
+   static final String DATABASE_URL = "jdbc:mysql://localhost/StudentSystem";
+   static final String DB_URL = "jdbc:mysql://localhost/";
+   
+   //Connections
    private static Connection conn = null;
    private static Statement statement = null;
-   
-   static JFrame frame = new JFrame("Student Insert");
-   
+
    
    // default query retrieves all data from authors table
    static final String DEFAULT_QUERY = "SELECT * "
@@ -177,7 +178,7 @@ public class StudentSystem extends JFrame
 	   Statement stmtDrop = conn.createStatement();
 	   stmtDrop.execute(queryDrop);
 	   
-	   //Create a database named Student
+	   //Create a database named StudentSystem
 	   System.out.println("Creating database...");
 	   statement = conn.createStatement();
 	   
@@ -186,15 +187,14 @@ public class StudentSystem extends JFrame
 	   System.out.println("Database created successfully");
    }
    
-   
-   
    private static void createTable() throws SQLException
    {
-	   //Open a connection and select the database named Student
+	   //Open a connection and select the database named StudentSystem
 	   System.out.println("Connecting to database...");
 	   conn = DriverManager.getConnection(DB_URL+"StudentSystem", USERNAME, PASSWORD);
 	   statement = conn.createStatement();
 	   
+	   //Drop all existing tables
 	   String queryDropStudent = "DROP TABLE IF EXISTS STUDENT";
 	   String queryDropExamResult = "DROP TABLE IF EXISTS EXAMRESULT";
 	   String queryDropSemester = "DROP TABLE IF EXISTS SEMESTER";
@@ -210,17 +210,18 @@ public class StudentSystem extends JFrame
 	   stmtDrop.execute(queryDropAttendance);
 	   stmtDrop.execute(queryDropLabReport);
 	   
+	   //Creating all tables for the StudentSystem database
 	   String createStudent = "CREATE TABLE STUDENT("
 			   + "ID INTEGER NOT NULL , "
 			   + "FirstName VARCHAR(20), "
 			   + "LastName VARCHAR(20), "
 			   + "Gender VARCHAR(6), "
-			   + "Grade VARCHAR(10), "
+			   + "Grade VARCHAR(15), "
 			   + "PRIMARY KEY (ID))";
 	   statement.execute(createStudent);
 	   
 	   String createTableExamResult = "CREATE TABLE EXAMRESULT("
-			   + "ID INTEGER NOT NULL REFERENCES STUDENT(ID), "
+			   + "ID INTEGER NOT NULL, "
 			   + "Score INTEGER DEFAULT 0, "
 			   + "LetterGrade CHAR NOT NULL DEFAULT 'E', "
 			   + "PRIMARY KEY (ID))";
@@ -237,7 +238,7 @@ public class StudentSystem extends JFrame
 	   String createTableExamInfo = "CREATE TABLE EXAMINFO("
 			   + "ID INTEGER NOT NULL, "
 			   + "Version INTEGER DEFAULT 1, "
-			   + "Date VARCHAR(10) DEFAULT '10-30-2013', "
+			   + "Date DATE DEFAULT '2013-10-01', "
 			   + "PRIMARY KEY (ID))";
 	   statement.execute(createTableExamInfo);
 	   
@@ -249,10 +250,10 @@ public class StudentSystem extends JFrame
 	   statement.execute(createTableAttendance);
 	   
 	   String createTableLabReport = "CREATE TABLE LABRESULT("
-			   + "ID INTEGER NOT NULL REFERENCES STUDENT(ID), "
+			   + "ID INTEGER NOT NULL, "
 			   + "LabLow INTEGER DEFAULT 0, "
 			   + "LabHigh INTEGER DEFAULT 0, "
-			   + "LabPassFail VARCHAR(5) DEFAULT 'N/A', "
+			   + "LabPassFail BOOLEAN DEFAULT 0, "
 			   + "PRIMARY KEY (ID))";
 			
 	   statement.execute(createTableLabReport);
@@ -263,16 +264,50 @@ public class StudentSystem extends JFrame
    
    private static void CreateTriggers() throws SQLException
    {
-	   String trigger1 = "DROP Trigger Updates; "
+	   String trigger1 = "DROP Trigger IF EXISTS Updates "
+	 		+ "delimiter // "
 	   		+ "CREATE Trigger Updates "
 	   		+ "AFTER INSERT of ID ON STUDENT "
 	   		+ "WHEN (new.ID not in (Select ID FROM STUDENT) "
 	   		+ "BEGIN "
-	   		+ "insert into EXAMINFO values (new.ID, 1, '10-30-2013'); "
+	   		+ "insert into EXAMINFO values (new.ID, 1, '2013-10-01'); "
 	   		+ "END;";
 	   statement.execute(trigger1);
+	   
+	   
+	   String trigger2 = "DROP TRIGGER IF EXISTS UpdatePassFail; "
+	   		+ "delimiter // "
+	   		+ "CREATE TRIGER UpdatePassFail; "
+	   		+ "AFTER INSERT ON ATTENDANCE "
+	   		+ "FOR EACH ROW "
+	   		+ "BEGIN "
+	   		+ "IF new.present = 0 AND Exam.Date = '2012-09-12' "
+	   		+ "THEN INSERT INTO EXAM RESULTS VALUES (0, 'F') "
+	   		+ "WHERE EXAMRESULT.ID = STUDENT.ID; "
+	   		+ "END IF; "
+	   		+ "END; "
+	   		+ "//delimiter; ";
+	   statement.execute(trigger2);
+	   
+	   String trigger3 = "DROP TRIGGER IF EXISTS ExtraCredit; "
+	   		+ "//delimiter "
+	   		+ "CREATE TRIGGER ExtraCredit; "
+	   		+ "AFTER UPDATE ON EXAMRESULT "
+	   		+ "FOR EACH ROW "
+	   		+ "BEGIN IF SCORE = 10 AND Score <> 11 "
+	   		+ "THEN UPDATE EXAMRESULT "
+	   		+ "SET SCORE = 11 "
+	   		+ "WHERE EXAMRESULT.ID = STUDENT.ID; "
+	   		+ "END IF; "
+	   		+ "END; "
+	   		+ "//delimiter; ";
+	   statement.execute(trigger3);
+	   		
+	   		
+			   
    }
    
+   //Load Data from previously created text file
    private static void loadDataIntoTable() throws SQLException
    {
 	   System.out.println("Load data from a file student.txt and examresult.txt");
@@ -290,16 +325,16 @@ public class StudentSystem extends JFrame
 	   statement.execute(loadDataSQLLabReport);
    }
    
-   
-   
-   
+   //GUI of commands
    public static void Buttons(){
 	   JFrame f = new JFrame("Student System Controls");
 	   JButton b1 = new JButton("Insert Student Information");
 	   JButton b2 = new JButton("Delete Student");
-	   JButton b3 = new JButton("Display All Query");
-	   JButton b4 = new JButton("Display Student Information");
-	   JButton b5 = new JButton("Display Exam/Score/Grade Information");
+	   JButton b3 = new JButton("View Detailed Info");
+	   JButton b4 = new JButton("View Student");
+	   JButton b5 = new JButton("View Exam Average Score");
+	   JButton b6 = new JButton("Number Registered Students");
+	   JButton b7 = new JButton("View Lab Scores");
 	   
 	   
 	   b1.addActionListener(new ActionListener(){
@@ -336,7 +371,7 @@ public class StudentSystem extends JFrame
 				public void actionPerformed(ActionEvent arg0) {	
 					try {
 						tableModel.setQuery("SELECT * "
-									+ "FROM STUDENT natural join SEMESTER natural join ATTENDANCE");
+									+ "FROM STUDENT");
 					} catch (IllegalStateException | SQLException e) {
 						e.printStackTrace();
 					}
@@ -348,8 +383,8 @@ public class StudentSystem extends JFrame
 
 				public void actionPerformed(ActionEvent arg0) {	
 					try {
-						tableModel.setQuery("SELECT * "
-									+ "FROM EXAMRESULT natural join EXAMINFO natural join LABRESULT");
+						tableModel.setQuery("SELECT avg(score) "
+									+ "FROM EXAMRESULT natural join Student");
 					} catch (IllegalStateException | SQLException e) {
 						e.printStackTrace();
 					}
@@ -357,8 +392,32 @@ public class StudentSystem extends JFrame
 				   
 			   });
 		 
-	   
-	   
+		 b6.addActionListener(new ActionListener(){
+
+				public void actionPerformed(ActionEvent arg0) {	
+					try {
+						tableModel.setQuery("SELECT count(distinct ID) "
+									+ "FROM Student");
+					} catch (IllegalStateException | SQLException e) {
+						e.printStackTrace();
+					}
+				}
+				   
+			   });
+		 
+		 b7.addActionListener(new ActionListener(){
+
+				public void actionPerformed(ActionEvent arg0) {	
+					try {
+						tableModel.setQuery("SELECT ID, LabLow, LabHigh "
+								+ "FROM LABRESULT");
+					} catch (IllegalStateException | SQLException e) {
+						e.printStackTrace();
+					}
+				}
+				   
+			   });
+
 	   f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	   Container content = f.getContentPane();
 	   content.setLayout(new FlowLayout());
@@ -367,14 +426,17 @@ public class StudentSystem extends JFrame
 	   content.add(b3);
 	   content.add(b4);
 	   content.add(b5);
+	   content.add(b6);
+	   content.add(b7);
 	   f.pack();
-	   f.setSize(400, 150);
+	   f.setSize(500, 200);
 	   f.setVisible(true); 
    }
    
    
    public static void CreateInputStudent(){
-	 	final String[] labels = {"ID: ", "FirstName: ", "LastName: ", "Gender: ", "Grade: ", "Term: ", "Year: ", "Present(0=no, 1=yes): ", "Number of Absences: "};
+	   final JFrame insertframe = new JFrame("Student Insert");
+	   final String[] labels = {"ID: ", "FirstName: ", "LastName: ", "Gender: ", "Grade: ", "Term: ", "Year: ", "Present(0=no, 1=yes): ", "Number of Absences: "};
 	 	final JTextField[] inputs = new JTextField[labels.length];
 	 	JPanel p = new JPanel(new SpringLayout());
 	 	
@@ -433,21 +495,22 @@ public class StudentSystem extends JFrame
 				} catch (SQLException e1) {
 					e1.printStackTrace();
 				}
-	 			frame.dispose();
+	 			insertframe.dispose();
 	 			
 	 		}
 	 	});
 	 	
 	 	
-	 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	 	frame.setContentPane(p);
-	 	frame.pack();
-	 	frame.setSize(500, 500);
-	 	frame.setVisible(true); 	
+	 	insertframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	 	insertframe.setContentPane(p);
+	 	insertframe.pack();
+	 	insertframe.setSize(500, 500);
+	 	insertframe.setVisible(true); 	
 }
    
    public static void CreateDeleteStudent(){
-	 	final String[] labels = {"FirstName: ", "LastName: "};
+	   final JFrame deleteframe = new JFrame("Student Delete");
+	   final String[] labels = {"FirstName: ", "LastName: "};
 	 	final JTextField[] deletes = new JTextField[labels.length];
 	 	JPanel p = new JPanel(new SpringLayout());
 	 	
@@ -490,17 +553,17 @@ public class StudentSystem extends JFrame
 				} catch (SQLException e1) {
 					e1.printStackTrace();
 				}
-	 			frame.dispose();
+	 			deleteframe.dispose();
 	 			
 	 		}
 	 	});
 	 	
 	 	
-	 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	 	frame.setContentPane(p);
-	 	frame.pack();
-	 	frame.setSize(300, 300);
-	 	frame.setVisible(true); 	
+	 	deleteframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	 	deleteframe.setContentPane(p);
+	 	deleteframe.pack();
+	 	deleteframe.setSize(300, 300);
+	 	deleteframe.setVisible(true); 	
 }   
    
    
@@ -512,7 +575,7 @@ public class StudentSystem extends JFrame
     	  Class.forName(JDBC_DRIVER); //Register JDBC Driver
     	  createDatabase();
     	  createTable();
-    	//  CreateTriggers();
+    //	  CreateTriggers();
     	  loadDataIntoTable();
     	  new StudentSystem();
     	  Buttons();
